10078fc8a7cb9c4b01f832bb7e22d870a9aceeda
giciarz412 <tomaszfefler1990@gmail.com>
2019-03-26 12:58:19 +0100

    mysql connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
 
 
+
+        String url = "jdbc:mysql://localhost:3306/mydb?autoReconnect=true&useSSL=false";
+        String user;
+        String password;
+        Connection myConn = DriverManager.getConnection(url, user, password);
 
     }
 }

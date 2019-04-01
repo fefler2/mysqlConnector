793b0c48682d2fce279a6b7fe84f0bc4c9a90cb5
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-01 19:46:50 +0200

    api nbp - jersey DZIALA!

--- src/main/java/Jersey/Waluty.java
+++ src/main/java/Jersey/Waluty.java
@@ -1,11 +1,26 @@
 package Jersey;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import java.util.zip.CRC32;
 
 public class Waluty {
     public static void main(String[] args) {
         try {
             Client klient = Client.create();
+            WebResource webZrodlo =
+                    klient.resource
+                            ("http://api.nbp.pl/api/exchangerates/rates/c/usd/today/");
+            ClientResponse webOdpowiedz = webZrodlo.accept("application/json")
+                    .get(ClientResponse.class);
+
+            if (webOdpowiedz.getStatus() != 200) {
+                throw new RuntimeException("Blad HTTP ...." + webOdpowiedz.getStatus());
+            }
+            String pobranyJson = webOdpowiedz.getEntity(String.class);
+            System.out.println(pobranyJson);
         }
         catch (Exception e){
             e.printStackTrace();

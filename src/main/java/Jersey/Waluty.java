9e827e666b2a631e66a1afea8a8c214315d7299b
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-01 19:35:50 +0200

    api nbp - jersey

---
+++ src/main/java/Jersey/Waluty.java
@@ -0,0 +1,15 @@
+package Jersey;
+
+import com.sun.jersey.api.client.Client;
+
+public class Waluty {
+    public static void main(String[] args) {
+        try {
+            Client klient = Client.create();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+}
